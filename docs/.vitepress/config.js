0efd53904d53728538ea041f016eef89eb8953ca
tcgmoo <jainmoksh@proton.me>
2022-06-15 20:07:11 +0800

    Add favicon, web app manifest, Windows tiles

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -117,6 +117,7 @@ export default defineConfig({
     themeConfig: {
         siteTitle: "The Blog of Random",
         lastUpdatedText: "Last updated on",
+        logo: '/favicon/favicon.ico',
 
         nav: [
             {
@@ -160,5 +161,16 @@ export default defineConfig({
         footer: {
             message: 'Released under the Unlicense.'
         }
-    }
+    },
+
+    head: [
+        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' }],
+        ['link', { rel: 'manifest', href: '/site.webmanifest'}],
+        ['link', { rel: 'mask-icon', href: '/favicon/safari-pinned-tab.svg', color: '#000000' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#42b883' }],
+        ['meta', { name: 'theme-color', content: '#ffffff' }]
+    ]
+
 })
