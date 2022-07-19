f6d5bc37053e527a2d96dfc3a899bca2c7a500b2
noClaps <jainmoksh@proton.me>
2022-07-19 22:15:27 +0800

    Add /blog/ to favicon links
    VitePress should do this automatically but they don't and it's disappointing.

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -72,11 +72,11 @@ export default {
     },
 
     head: [
-        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: './favicon/apple-touch-icon.png' }],
-        ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: './favicon/favicon-32x32.png' }],
-        ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: './favicon/favicon-16x16.png' }],
-        ['link', { rel: 'mask-icon', href: './favicon/safari-pinned-tab.svg', color: '#000000' }],
-        ['link', { rel: 'shortcut-icon', href: './favicon/favicon.ico' }]
+        ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/blog/favicon/apple-touch-icon.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/blog/favicon/favicon-32x32.png' }],
+        ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/blog/favicon/favicon-16x16.png' }],
+        ['link', { rel: 'mask-icon', href: '/blog/favicon/safari-pinned-tab.svg', color: '#000000' }],
+        ['link', { rel: 'shortcut-icon', href: '/blog/favicon/favicon.ico' }]
     ]
 
 }
