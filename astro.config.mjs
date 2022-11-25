dd776f69c78939919554454e92b091ffaa97f78a
noClaps <jainmoksh@proton.me>
2022-11-25 05:48:14 +0000

    Add service worker and PWA support
    Also moved favicons to src directory

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from 'astro-robots-txt';
 import critters from "astro-critters";
 import mdx from "@astrojs/mdx";
+import serviceWorker from "astrojs-service-worker";
+import webmanifest from 'astro-webmanifest';
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +13,13 @@ export default defineConfig({
   experimental: {
     integrations: true
   },
-  integrations: [robotsTxt(), sitemap(), critters(), mdx(), compress()]
+  integrations: [mdx(), serviceWorker(), webmanifest({
+    name: "The Blog of Random",
+    icon: "src/icon/favicon.svg",
+    description: "A blog about the most random things you can think of, brought to you by the most interesting boring person you've ever met.",
+    start_url: "/",
+    theme_color: "#013",
+    background_color: "#def",
+    display: "fullscreen"
+  }), robotsTxt(), sitemap(), critters(), compress()]
 });
