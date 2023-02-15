5ba6ff5db01505e4d88540b9c3566795ee9277ac
noClaps <github@zerolimits.dev>
2023-02-15 11:45:18 +0000

    Remove astro-critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
-import critters from "astro-critters";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.zerolimits.dev",
-  integrations: [mdx(), sitemap(), critters(), compress()]
+  integrations: [mdx(), sitemap(), compress()]
 });
