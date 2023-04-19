e39462a13bb6b1e256e269694e6a464b3ec70bc6
noClaps <github@zerolimits.dev>
2023-04-19 23:19:33 +0100

    Remove astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
-import compress from "astro-compress";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.zerolimits.dev",
-  integrations: [mdx(), sitemap(), compress()]
+  integrations: [mdx(), sitemap()]
 });
