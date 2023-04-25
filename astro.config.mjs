b1e78cdfbd9ec06593e7a0594c93cb58c350dddd
noClaps <github@zerolimits.dev>
2023-04-25 16:32:16 +0000

    Use rehype plugins for autolinking headings
    Closes [IMPROVE] Make headings hyperlinks during build #89

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,15 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import remarkMath from 'remark-math';
 import rehypeMathJax from 'rehype-mathjax';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import { rehypeHeadingIds } from '@astrojs/markdown-remark'
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.zerolimits.dev",
   markdown: {
     remarkPlugins: [remarkMath],
-    rehypePlugins: [rehypeMathJax]
+    rehypePlugins: [rehypeHeadingIds, rehypeMathJax, [rehypeAutolinkHeadings, {behavior: "wrap"}]]
   },
   integrations: [mdx(), sitemap()]
 });
