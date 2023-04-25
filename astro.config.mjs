c8fc0efd1fab3d7ba7c868a3371326b2eafe6ec3
noClaps <github@zerolimits.dev>
2023-04-25 16:17:33 +0000

    Use remark and rehype plugins to display math

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
+import remarkMath from 'remark-math';
+import rehypeMathJax from 'rehype-mathjax';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.zerolimits.dev",
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathJax]
+  },
   integrations: [mdx(), sitemap()]
 });
