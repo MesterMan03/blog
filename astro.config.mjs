d4ddaaa6ec18c9737251af3cc1af36b494e0fcd9
noClaps <github@zerolimits.dev>
2023-05-24 13:44:35 +0100

    Add reading time to psots

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,13 @@ import remarkMath from 'remark-math';
 import rehypeMathJax from 'rehype-mathjax';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.zerolimits.dev",
   markdown: {
-    remarkPlugins: [[remarkMath, { singleDollarTextMath: false }]],
+    remarkPlugins: [[remarkMath, { singleDollarTextMath: false }], remarkReadingTime],
     rehypePlugins: [rehypeHeadingIds, rehypeMathJax, [rehypeAutolinkHeadings, { behavior: "wrap" }]]
   },
   integrations: [mdx(), sitemap()],
